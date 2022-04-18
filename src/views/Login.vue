d6464a8ebd0943b4540dfd9b516f2895bab3ae9b
崔峰 <xiaolifeizei@163.com>
2022-04-18 16:53:13 +0800

    初始化

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <el-form ref="form" :model="loginForm">
+            <h3>欢迎登陆</h3>
+            <el-form-item>
+                <el-input type="text" autocomplete="false" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input type="password" autocomplete="false" v-model="loginForm.password"
+                          placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-checkbox v-model="checked">记住我</el-checkbox>
+            <el-button type="primary" style="width:100%">登陆</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                checked: false,
+                loginForm: {
+                    username: "admin",
+                    password: "123",
+                },
+            };
+        },
+        components: {},
+    };
+</script>
+
+<style>
+</style>
